77d361d3e10ebafc85f1b9011f718365d67e3767
natanael-cahya <natanaelmc60@gmail.com>
2021-10-02 14:06:25 +0700

    add healthy Checker

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module futsal-app
 go 1.16
 
 require (
+	github.com/hellofresh/health-go/v4 v4.5.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
--- routes/routes.go
+++ routes/routes.go
@@ -2,3 +2,24 @@ package routes
 
 // ini file untuk define semua routing
 // yang nantinya akan merefer ke masing masing route domain
+import (
+	"net/http"
+	"time"
+
+	"github.com/hellofresh/health-go/v4"
+	healthMysql "github.com/hellofresh/health-go/v4/checks/mysql"
+)
+
+func HealthChecks() {
+	h, _ := health.New()
+	h.Register(health.Config{
+		Name:      "Mysql-Checks",
+		Timeout:   time.Second * 2,
+		SkipOnErr: false,
+		Check: healthMysql.New(healthMysql.Config{
+			DSN: "root@tcp(127.0.0.1:3306)/db_futsal?charset=utf8mb4",
+		}),
+	})
+	http.Handle("/status", h.Handler())
+	http.ListenAndServe(":3000", nil)
+}
