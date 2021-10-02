25dd9d99456912347213069679a985054b45a7ca
natanael-cahya <natanaelmc60@gmail.com>
2021-10-02 15:59:23 +0700

    Solved miss placement

--- app/start.go
+++ app/start.go
@@ -1,9 +1,16 @@
 package app
 
+import (
+	"github.com/labstack/echo"
+)
+
 // file ini untuk init semua keperluan api
 // seperti koneksi database, routing, dll
 
 func Start() {
 	// panggil function ConnectToDB
 	ConnectToDB()
+	e := echo.New()
+	e.Start(":3000")
+
 }
